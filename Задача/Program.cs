953baa46d88a276194acee4ab78d5b6eea2a01e7
Svetlana M <jygik91@gmail.com>
2022-11-11 14:06:28 +0700

    Добавлен метод для решения

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿string[] decr(string[]sA)
+{
+int L = 0;
+foreach(string s in sA)
+if (s.Length<=3) L++;
+string[] NewArray = new string[sA.Length];
+int i = 0;
+foreach(string s in sA)
+if(s.Length<=3)
+{
+    NewArray[i] = s;
+    i++;
+}
+return NewArray;
+}
